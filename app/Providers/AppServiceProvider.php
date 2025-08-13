f3bcc21e4395088f23c06d0a9b903f57226efe01
Danny Spangenberg <danny.spangenberg@twiceware.de>
2025-08-13 18:56:52 +0200

    ...

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,9 +14,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        if ($this->app->environment() !== 'production') {
-            $this->app->register(IdeHelperServiceProvider::class);
-        }
     }
 
     /**
