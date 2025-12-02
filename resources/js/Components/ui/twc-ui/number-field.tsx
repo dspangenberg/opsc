bc2412302c281ba85e356a7498aa56cb7ce9202f
Danny Spangenberg <danny.spangenberg@twiceware.de>
2025-12-02 03:01:41 +0100

    Field, NumberField und TextField mit twc-ui abgeglichen

--- resources/js/Components/ui/twc-ui/number-field.tsx
+++ resources/js/Components/ui/twc-ui/number-field.tsx
@@ -9,10 +9,10 @@ import {
   composeRenderProps,
   Text
 } from 'react-aria-components'
+import { useFormContext } from './form'
 import { cn } from '@/Lib/utils'
 import { Button } from './button'
 import { FieldError, FieldGroup, Label } from './field'
-import { useFormContext } from './form'
 
 const BaseNumberField = AriaNumberField
 
@@ -26,7 +26,7 @@ const NumberFieldInput = ({ className, ...props }: AriaInputProps) => {
     <AriaInput
       className={composeRenderProps(className, className =>
         cn(
-          'mr-2 w-fit min-w-0 flex-1 border-0 border-transparent bg-background pr-4 pl-0 text-right text-sm outline-0 placeholder:text-muted-foreground [&::-webkit-search-cancel-button]:hidden',
+          'mr-2 w-fit min-w-0 flex-1 border-0 border-transparent bg-background pr-4 pl-0 text-right text-sm outline-0 placeholder:text-muted-foreground [&::-webkit-search-cancel-button]:hidden ',
           className
         )
       )}
@@ -36,18 +36,14 @@ const NumberFieldInput = ({ className, ...props }: AriaInputProps) => {
   )
 }
 
-const NumberFieldSteppers = ({
-  className,
-  isDisabled,
-  ...props
-}: React.ComponentProps<'div'> & { isDisabled?: boolean }) => {
+const NumberFieldSteppers = ({ className, ...props }: React.ComponentProps<'div'>) => {
   return (
     <div className={cn('absolute right-0 flex h-full flex-col border-l', className)} {...props}>
-      <NumberFieldStepper slot="increment" isDisabled={isDisabled}>
+      <NumberFieldStepper slot="increment">
         <ChevronUp aria-hidden className="size-4" />
       </NumberFieldStepper>
       <div className="border-b" />
-      <NumberFieldStepper slot="decrement" isDisabled={isDisabled}>
+      <NumberFieldStepper slot="decrement">
         <ChevronDown aria-hidden className="size-4" />
       </NumberFieldStepper>
     </div>
@@ -60,7 +56,6 @@ const NumberFieldStepper = ({ className, ...props }: AriaButtonProps) => {
       className={composeRenderProps(className, className =>
         cn('w-auto grow rounded-none px-0.5 text-muted-foreground', className)
       )}
-      disabled={props.isDisabled}
       variant={'ghost'}
       size={'icon'}
       {...props}
@@ -73,8 +68,6 @@ interface NumberFieldProps extends Omit<AriaNumberFieldProps, 'value' | 'onChang
   description?: string
   onChange?: ((value: number | null) => void) | ((value: number) => void)
   value?: number | null | undefined
-  isReadonly?: boolean
-  isDisabled?: boolean // Neue isDisabled Prop
   error?: string | undefined
   name?: string
 }
@@ -85,8 +78,6 @@ const NumberField = ({
   className,
   formatOptions,
   isRequired = false,
-  isReadonly = false,
-  isDisabled = false, // Neue isDisabled Prop mit Standardwert
   isInvalid = false,
   onChange,
   value,
@@ -118,17 +109,15 @@ const NumberField = ({
         cn('group flex flex-col gap-1.5', className)
       )}
       isInvalid={hasError}
-      isReadOnly={isReadonly}
-      isDisabled={isDisabled} // isDisabled an BaseNumberField weiterleiten
       formatOptions={formatOptions}
       value={value ?? undefined}
       onChange={handleChange}
       {...props}
     >
-      {label && <Label value={label} isRequired={isRequired} />}
+      <Label value={label} isRequired={isRequired}  />
       <FieldGroup isInvalid={hasError}>
-        <NumberFieldInput className="outline:0 focus:ring-0" />
-        <NumberFieldSteppers isDisabled={isDisabled || isReadonly} />
+        <NumberFieldInput  />
+        <NumberFieldSteppers />
       </FieldGroup>
       {description && (
         <Text className="text-muted-foreground text-sm" slot="description">
