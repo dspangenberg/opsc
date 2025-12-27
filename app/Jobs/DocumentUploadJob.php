25c97b1449582d45f4269f17ca12612ddbcd554a
Danny Spangenberg <danny.spangenberg@twiceware.de>
2025-12-27 04:36:23 +0100

    Dokument-Upload in Service/Job verschoben, MultiDoc-Upload

---
+++ app/Jobs/DocumentUploadJob.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Services\MultidocService;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Queue\Queueable;
+use Exception;
+
+class ProcessMultiDocJob implements ShouldQueue
+{
+    use Queueable;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(
+        public string $file
+    ) {}
+
+    /**
+     * Execute the job.
+     *
+     * @throws Exception
+     */
+    public function handle(MultidocService $service): void
+    {
+        $service->process($this->file);
+    }
+}
---
+++ app/Jobs/ProcessMultiDocJob.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Services\ReceiptService;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Queue\Queueable;
+use Exception;
+
+class ReceiptUploadJob implements ShouldQueue
+{
+    use Queueable;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(
+        public string $file
+    ) {}
+
+    /**
+     * Execute the job.
+     *
+     * @throws Exception
+     */
+    public function handle(ReceiptService $service): void
+    {
+        $service->processZipArchive($this->file);
+    }
+}
