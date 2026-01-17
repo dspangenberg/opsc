943542eddc0cfa226f77e7d0d9c6f5251755794f
Danny Spangenberg <danny.spangenberg@twiceware.de>
2026-01-17 19:15:16 +0100

    - Leeres Erfolgskonto erlauben

--- resources/js/Pages/App/Contact/ContactDetailsPersons.tsx
+++ resources/js/Pages/App/Contact/ContactDetailsPersons.tsx
@@ -2,8 +2,6 @@ import { usePage } from '@inertiajs/react'
 import type * as React from 'react'
 import { DataTable } from '@/Components/DataTable'
 import { ContactDetailsLayout } from '@/Pages/App/Contact/ContactDetailsLayout'
-import { ContactDetailsOrg } from '@/Pages/App/Contact/ContactDetailsOrg'
-import { ContactDetailsPerson } from '@/Pages/App/Contact/ContactDetailsPerson'
 import { columns } from '@/Pages/App/Contact/ContactDetailsPersonsColumns'
 import type { PageProps } from '@/Types'
 
