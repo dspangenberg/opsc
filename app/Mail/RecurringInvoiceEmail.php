b58fdbc52d27b767c5c34a1be1d0740011bc8cc8
Danny Spangenberg <danny.spangenberg@twiceware.de>
2026-01-24 15:38:03 +0100

    - E-Mail-Benachrichtigung, wenn eine neue wiederkehrende Rechnung erstellt wurde.

---
+++ app/Mail/RecurringInvoiceEmail.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Invoice;
+use Illuminate\Bus\Queueable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class RecurringInvoiceEmail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public Invoice $invoice;
+
+    public function __construct(Invoice $invoice)
+    {
+        $this->invoice = $invoice;
+    }
+
+    public function build()
+    {
+        $invoice = $this->invoice;
+
+        return $this->subject('opsc.cloud - Neue E-Mail-Adresse bestaetigen')
+            ->view('generated.recurring-invoice', [
+                'invoice' => $invoice
+            ]);
+    }
+}
