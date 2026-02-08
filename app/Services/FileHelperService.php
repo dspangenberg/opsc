ab44e770ccaa94a8492fc6165b6be2e9dbcf5758
Danny Spangenberg <danny.spangenberg@twiceware.de>
2026-02-08 21:20:48 +0100

    - Sald bei Buchungen und Unteansicht nach Konto

--- app/Services/FileHelperService.php
+++ app/Services/FileHelperService.php
@@ -10,23 +10,20 @@ namespace App\Services;
 
 use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Str;
-use Spatie\TemporaryDirectory\TemporaryDirectory;
 use Log;
+use Spatie\TemporaryDirectory\TemporaryDirectory;
 
 class FileHelperService
 {
     public function createTempFile(UploadedFile $file): string
     {
-
-        ds($file);
         $tmpDir = TemporaryDirectory::make();
         $fileName = Str::random(32).'.'.$file->getClientOriginalExtension();
         $file->move($tmpDir->path(), $fileName);
 
+        $f = $tmpDir->path().'/'.$fileName;
+        Log::info('tmpFile: '.$f);
 
-        $f = $tmpDir->path() . '/'.$fileName;
-        Log::info('tmpFile: ' .$f);
-        ds($f);
         return $f;
     }
 
@@ -38,12 +35,13 @@ class FileHelperService
     public function createTemporaryFileFromDoc($fileName, $content): string
     {
         $tempFile = storage_path('app/temp');
-        if (!file_exists($tempFile)) {
+        if (! file_exists($tempFile)) {
             mkdir($tempFile, 0755, true);
         }
         $fileName = uniqid().'_'.$fileName.'.pdf';
         file_put_contents($tempFile.'/'.$fileName, $content);
         $realPath = $tempFile.'/'.$fileName;
+
         return $realPath;
     }
 }
