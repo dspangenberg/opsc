624850d715e6c62e215e89ab05a514a90b0b3036
Danny Spangenberg <danny.spangenberg@twiceware.de>
2026-02-09 01:32:48 +0100

    - Buchungenkorrigieren

---
+++ app/Http/Requests/CorrectBookingsRequest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CorrectBookingsRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'ids' => ['required', 'string', 'regex:/^[0-9]+(,[0-9]+)*$/'],
+        ];
+    }
+
+    /**
+     * Get custom error messages for validator errors.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'ids.required' => 'Es müssen Buchungen ausgewählt werden.',
+            'ids.regex' => 'Das Format der Buchungs-IDs ist ungültig.',
+        ];
+    }
+
+    /**
+     * Get the booking IDs as an array of integers.
+     *
+     * @return array<int>
+     */
+    public function getBookingIds(): array
+    {
+        $ids = explode(',', $this->input('ids', ''));
+
+        return array_values(array_filter(array_map('intval', $ids), fn ($id) => $id > 0));
+    }
+}
