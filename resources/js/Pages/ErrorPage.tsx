c46dce17981741d3d1a30647af71ea0f7342697d
Danny Spangenberg <danny.spangenberg@twiceware.de>
2026-02-17 14:50:06 +0100

    - Error-Pages - Sentry

---
+++ resources/js/Pages/ErrorPage.tsx
@@ -0,0 +1,25 @@
+const ErrorPage = ({ status }: { status: 403 | 404 | 500 | 503 }) => {
+  const title = {
+    503: '503: Service nicht verfügbar',
+    500: '500: Serverfehler',
+    404: '404: Seite nicht gefunden',
+    403: '403: Zugriff verweigert'
+  }[status]
+
+  const description = {
+    503: 'Entschuldigung, wir führen gerade Wartungsarbeiten durch. Bitte versuchen Sie es in Kürze erneut.',
+    500: 'Hoppla, auf unserem Server ist ein Fehler aufgetreten.',
+    404: 'Entschuldigung, die gesuchte Seite konnte nicht gefunden werden.',
+    403: 'Entschuldigung, Sie haben keine Berechtigung, auf diese Seite zuzugreifen.'
+  }[status]
+
+  return (
+    <div>
+      <h1>{title}</h1>
+      <div>{description}</div>
+    </div>
+  )
+}
+
+export default ErrorPage
+export { ErrorPage }
