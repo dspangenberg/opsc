a2ea5963996b27f0e3d875e7d2a62a22c978a694
Danny Spangenberg <danny.spangenberg@twiceware.de>
2026-07-04 12:10:37 +0200

    Packages aktualisiert

--- resources/js/Components/twc-ui/form-grid.tsx
+++ resources/js/Components/twc-ui/form-grid.tsx
@@ -10,6 +10,7 @@ interface FormLayoutGroupProps {
   title?: string
   className?: string
   action?: React.ReactNode
+  description?: React.ReactNode | string
   titleClass?: string
   children?: React.ReactNode
 }
@@ -19,12 +20,12 @@ export const FormGrid: React.FC<FormLayoutGroupProps> = ({
   cols = 24,
   fullWidth = true,
   action = null,
-
+  description = undefined,
   grid = true,
   margin = true,
   title = '',
   className = '',
-  titleClass = 'font-medium text-sm text-black mt-4 pb-3 border-b',
+  titleClass = 'font-medium text-sm text-black pt-4 pb-3',
   children
 }) => {
   const gridCols = {
@@ -34,17 +35,22 @@ export const FormGrid: React.FC<FormLayoutGroupProps> = ({
   }[cols]
 
   return (
-    <div className="flex-1 border-accent">
+    <div className="flex-1">
       {title !== '' && (
-        <div className={cn('flex items-center px-4', titleClass)}>
-          <div className="flex-1">{title}</div>
-          <div className="flex-none">{action}</div>
+        <div className="flex flex-1 flex-col border-b py-0">
+          <div className={cn('flex items-center px-4', titleClass)}>
+            <div className="flex-1">{title}</div>
+            <div className="flex-none">{action}</div>
+          </div>
+          {description && (
+            <div className="-mt-1 flex-1 px-4 pb-2 text-foreground/60 text-xs">{description}</div>
+          )}
         </div>
       )}
       <div
         className={cn(
-          'mb-1 flex-1 px-4 last:mb-3',
-          border ? 'mx-0 border-accent border-t pt-4' : '',
+          'flex-1 px-4 pt-4 last:mb-3',
+          border ? 'border/50 mx-0 mt-4 mb-4 border-t' : '',
           grid ? 'm-0 grid gap-x-3 gap-y-6 px-4 py-2' : '',
           margin ? 'mt-3' : 'not-first:mt-2',
           grid ? gridCols : '',
